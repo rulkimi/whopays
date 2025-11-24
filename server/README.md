fb331d2a8729c0650a1b21830d38999637f393eb
rulkimi <azrulkimi00@gmail.com>
2025-11-24 17:50:49 +0800

    fix: remove unnecessary try/except

--- server/README.md
+++ server/README.md
@@ -3,6 +3,7 @@
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+docker compose up -d (first setup)
 alembic upgrade init (if no alembic folder generated)
 alembic upgrade head
 alembic/sripts/arev.sh "explanation" (to generate migrations)
--- server/app/modules/user/router.py
+++ server/app/modules/user/router.py
@@ -11,22 +11,15 @@ def register_user(
 	user_data: UserCreate,
 	user_service: UserService = Depends(get_user_service)
 ):
-  try:
-    new_user = user_service.register(user_data=user_data)
-    if not new_user:
-      return APIResponse.error(
-        message=f"User with email {user_data.email} already exists.",
-        code=status.HTTP_409_CONFLICT
-      )
-    
-    return APIResponse.success(
-      message="User registered successfully",
-      data=new_user,
-      code=status.HTTP_201_CREATED
-    )
-
-  except Exception as e:
+  new_user = user_service.register(user_data=user_data)
+  if not new_user:
     return APIResponse.error(
-      message=f"Unable to register user right now: {e}",
-      code=status.HTTP_500_INTERNAL_SERVER_ERROR
+      message=f"User with email {user_data.email} already exists.",
+      code=status.HTTP_409_CONFLICT
     )
+  
+  return APIResponse.success(
+    message="User registered successfully",
+    data=new_user,
+    code=status.HTTP_201_CREATED
+  )
